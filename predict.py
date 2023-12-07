82643022f765214552897712320ccd864c6fd2cb
Angelo Lamonaca <angelo.lamonaca@gmail.com>
2023-12-07 16:55:31 +0100

    Implements image encryption

--- predict.py
+++ predict.py
@@ -6,15 +6,80 @@ from typing import Optional
 
 import cv2
 import numpy as np
-from PIL import Image
 from basicsr.archs.rrdbnet_arch import RRDBNet
 from cog import BasePredictor, Input, Path
 from gfpgan import GFPGANer
 from realesrgan import RealESRGANer
+import requests
+import io
+from PIL import Image, ImageDraw
+import random
+import string
 
 MODEL_NAME = "RealESRGAN_x4plus"
 ESRGAN_PATH = os.path.join("/root/.cache/realesrgan", MODEL_NAME + ".pth")
 GFPGAN_PATH = "/root/.cache/realesrgan/GFPGANv1.3.pth"
+
+
+def upload_pil_image(pil_image, mime_type):
+    # Convert PIL Image to byte array
+    img_byte_arr = io.BytesIO()
+    pil_image.save(img_byte_arr, format="PNG")
+    img_byte_arr = img_byte_arr.getvalue()
+
+    # S3 API Endpoints
+    S3_UPLOAD_API_ENDPOINT = 'https://4yt4k9lwkd.execute-api.us-east-1.amazonaws.com/default/neuramare-uploads-genvista-client-s3-upload-getPresignedUrl'
+    S3_DOWNLOAD_API_ENDPOINT = 'https://z2mfup4u36.execute-api.us-east-1.amazonaws.com/default/neuramare-uploads-genvista-client-s3-download-getPresignedUrl'
+
+    # Step 1: Get the S3 upload URL
+    response = requests.get(S3_UPLOAD_API_ENDPOINT)
+    s3_upload_pre_signed_url_response = response.json()
+    print('Uploading to: ', s3_upload_pre_signed_url_response['uploadURL'])
+
+    # Step 2: Upload the image to S3
+    upload_url = s3_upload_pre_signed_url_response['uploadURL']
+    headers = {'Content-Type': mime_type}
+    requests.put(upload_url, data=img_byte_arr, headers=headers)
+
+    # Step 3: Get the S3 download URL
+    response = requests.post(S3_DOWNLOAD_API_ENDPOINT, json={'key': s3_upload_pre_signed_url_response['key']})
+    s3_download_pre_signed_url_response = response.json()
+    return s3_download_pre_signed_url_response['imageUrl']
+
+
+def post_to_ai_service(job_id, image_url):
+    # AI Service Endpoint
+    AI_SERVICE_ENDPOINT = 'https://ai-service-dev.neuramare.com/v1/replicate/image'
+
+    # Payload for the POST request
+    payload = {
+        'id': job_id,
+        'imageUrl': image_url
+    }
+
+    # Perform the POST request
+    response = requests.post(AI_SERVICE_ENDPOINT, json=payload)
+
+    # Check if the response status is not 200
+    if response.status_code != 200:
+        # Raise an exception with the status code and response text
+        raise Exception(f"NSFW content detected. Try running it again, or try a different prompt.")
+
+    return
+
+
+def create_fake_image(width, height):
+    # Create a blank image with a solid color (e.g., white)
+    image = Image.new('RGB', (width, height), color='white')
+
+    # Generate a random string
+    random_text = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+
+    # Draw the random text on the image
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), random_text, fill='black')
+
+    return image
 
 
 def crop_to_exact_size(image, target_width, target_height):
@@ -75,6 +140,9 @@ class Predictor(BasePredictor):
             height: int = Input(
                 description="Desired height of the output image", default=None
             ),
+            job_id: str = Input(
+                description="job id", default=None
+            ),
             face_enhance: bool = Input(
                 description="Run GFPGAN face enhancement along with upscaling",
                 default=False,
@@ -103,10 +171,47 @@ class Predictor(BasePredictor):
                 output = Image.fromarray(output)
             # Resize the image to the original size before saving
             output = crop_to_exact_size(output, width, height)
+            if job_id:
+                if not isinstance(output, Image.Image):
+                    if output.dtype != np.uint8:
+                        output = output.astype(np.uint8)
+                    # Assuming the image was in BGR format, convert it to RGB.
+                    if output.shape[-1] == 3:  # Check if the image has three channels
+                        output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+                    output = Image.fromarray(output)
+                print("Upload image")
+                uploaded_image_url = upload_pil_image(output, "image/png")
+                print("Send url to ai service for saving")
+                post_to_ai_service(job_id, uploaded_image_url)
+                print("Define the output path")
+                save_path = os.path.join(tempfile.mkdtemp(), "output.png")
+                print("Create fake image, since we did the upload")
+                fake_image = create_fake_image(100, 100)
+                fake_image.save(save_path)
+                return Path(save_path)
+            else:
+                save_path = os.path.join(tempfile.mkdtemp(), "output.png")
+                output.save(save_path)
+                return Path(save_path)
+        if job_id:
+            if not isinstance(output, Image.Image):
+                if output.dtype != np.uint8:
+                    output = output.astype(np.uint8)
+                # Assuming the image was in BGR format, convert it to RGB.
+                if output.shape[-1] == 3:  # Check if the image has three channels
+                    output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+                output = Image.fromarray(output)
+            print("Upload image")
+            uploaded_image_url = upload_pil_image(output, "image/png")
+            print("Send url to ai service for saving")
+            post_to_ai_service(job_id, uploaded_image_url)
+            print("Define the output path")
             save_path = os.path.join(tempfile.mkdtemp(), "output.png")
-            output.save(save_path)
+            print("Create fake image, since we did the upload")
+            fake_image = create_fake_image(100, 100)
+            fake_image.save(save_path)
             return Path(save_path)
-
-        save_path = os.path.join(tempfile.mkdtemp(), "output.png")
-        cv2.imwrite(save_path, output)
-        return Path(save_path)
+        else:
+            save_path = os.path.join(tempfile.mkdtemp(), "output.png")
+            cv2.imwrite(save_path, output)
+            return Path(save_path)
